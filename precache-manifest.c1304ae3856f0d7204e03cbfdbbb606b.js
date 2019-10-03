557d85f7350ec1a762767c0a3c95c3a08e813d77
cdgeorge7 <cdgeorge7@gmail.com>
2019-10-02 23:48:22 -0500

    Updates

---
+++ precache-manifest.c1304ae3856f0d7204e03cbfdbbb606b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97ab0bde292f9fcc5b75daba15deb33",
+    "url": "/dfs-apps-frontend-react/index.html"
+  },
+  {
+    "revision": "b369099ee0f6bac37c33",
+    "url": "/dfs-apps-frontend-react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f3c9caa84809f7331234",
+    "url": "/dfs-apps-frontend-react/static/css/main.78f4adb3.chunk.css"
+  },
+  {
+    "revision": "b369099ee0f6bac37c33",
+    "url": "/dfs-apps-frontend-react/static/js/2.1ba7060f.chunk.js"
+  },
+  {
+    "revision": "f3c9caa84809f7331234",
+    "url": "/dfs-apps-frontend-react/static/js/main.d85ebbad.chunk.js"
+  },
+  {
+    "revision": "2d052ff58c7f0ef67b88",
+    "url": "/dfs-apps-frontend-react/static/js/runtime~main.828a68ca.js"
+  }
+]);
